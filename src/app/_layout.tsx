66f15f94c5d2e2c9f2118c15abf9fa955a5e93d9
Franklyn Rocha <faranresolve@gmail.com>
2024-03-28 17:57:12 -0300

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,34 @@
+import { Slot } from "expo-router";
+import { StatusBar } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+//mostra a splashscreen
+SplashScreen.preventAutoHideAsync();
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (fontsLoaded) {
+    //oculta a splashscreen
+    SplashScreen.hideAsync();
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar barStyle="light-content" />
+      {fontsLoaded && <Slot />}
+    </GestureHandlerRootView>
+  );
+}
